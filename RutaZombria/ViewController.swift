04560b2ce8bdef28d82b1a4e26cf20b5303969af
Valentin Granados <valefcfhc@gmail.com>
2015-10-15 21:29:25 -0500

    location Manager

--- RutaZombria/ViewController.swift
+++ RutaZombria/ViewController.swift
@@ -10,13 +10,46 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var Map: MKMapView!
+    var annotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    var span = MKCoordinateSpanMake(0.02, 0.02)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization() //requestWhenInUseAuthorization() NSLocationWhenInUseUsageDescription in .plist
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Current location lon: \(locations.last?.coordinate.longitude)")
+        print("Current location lat: \(locations.last?.coordinate.latitude)")
+        
+        locationManager.stopUpdatingLocation()
+        
+        let region = MKCoordinateRegion(center: (locations.last?.coordinate)!, span: span)
+        
+        Map.setRegion(region, animated: true)
+        
+        annotation.coordinate = CLLocationCoordinate2DMake(
+                                        (locations.last?.coordinate.latitude)!,
+                                        (locations.last?.coordinate.longitude)!   )
+        annotation.title = "Me"
+        annotation.subtitle = "vale me"
+        
+        Map.addAnnotation(annotation)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error finding location: " + error.localizedDescription)
     }
 
     override func didReceiveMemoryWarning() {
