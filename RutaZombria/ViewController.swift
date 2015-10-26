ca7462ac08468f855f9ebfc2bf50f04dbdf0a5bc
Valentin Granados <valefcfhc@gmail.com>
2015-10-25 20:04:21 -0600

    Pin MKAnnotationView with image!

--- RutaZombria/ViewController.swift
+++ RutaZombria/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var Map: MKMapView!
-    var annotation = MKPointAnnotation()
+    //var annotation = MKPointAnnotation()
     let locationManager = CLLocationManager()
     var span = MKCoordinateSpanMake(0.02, 0.02)
+    var pinView: MKAnnotationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +24,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization() //requestWhenInUseAuthorization() NSLocationWhenInUseUsageDescription in .plist
+        //locationManager.requestAlwaysAuthorization() //requestWhenInUseAuthorization() NSLocationWhenInUseUsageDescription in .plist
         locationManager.startUpdatingLocation()
         
+        Map.delegate = self
         
     }
     
@@ -39,17 +41,50 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         Map.setRegion(region, animated: true)
         
-        annotation.coordinate = CLLocationCoordinate2DMake(
+        /*annotation.coordinate = CLLocationCoordinate2DMake(
                                         (locations.last?.coordinate.latitude)!,
                                         (locations.last?.coordinate.longitude)!   )
         annotation.title = "Me"
         annotation.subtitle = "vale me"
         
-        Map.addAnnotation(annotation)
+        Map.addAnnotation(annotation)*/
+        
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print("Error finding location: " + error.localizedDescription)
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "zombie"
+        
+        pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as MKAnnotationView!
+        
+        if pinView == nil {
+            pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView.image = UIImage(named:"zombie")
+            pinView.canShowCallout = true
+           
+        }
+        else {
+            pinView.annotation = annotation
+        }
+        
+        //Map.addAnnotation(pinView.annotation!)
+        
+        return pinView
+        
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        
+        
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
